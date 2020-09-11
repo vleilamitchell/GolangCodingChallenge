c9242846e49de64f4ac9e4cf0c827667ab6a757c
vleilamitchell <vleilamitchell@gmail.com>
2020-09-10 23:13:38 -0400

    Documentation commit.

--- Readme.md
+++ Readme.md
@@ -1,8 +1,19 @@
 **GoLang Codig Challenge**
 
-This is a simple HTTP REST endpoint which takes a URL encoded IP address against a known list of whitelisted countries and responds as to whether that IP is authorized to continue. The service uses GeoLite2 data to determine IP address origin.
+This is a simple HTTP REST endpoint which takes a URL encoded IP address against a known list of whitelisted countries 
+and responds as to whether that IP is authorized to continue. The service uses GeoLite2 data to determine IP address origin.
 
-If the geoipupdate utility is installed the default installation of this service will update the built-in version of the GeoLite2-Country.mmdb data set.
+If the geoipupdate utility is installed the default installation of this service will update the built-in version of 
+the GeoLite2-Country.mmdb data set.
 
+**Usage**
+
+Submit a POST request to _http:\\\\[hostname]:10000\processIpRequest_ with the following format in the body: 
+
+`{
+   "request":"8.8.8.8",
+   "whitelist": ["United States","Canada"]
+ }`
+ 
 This product includes GeoLite2 data created by MaxMind, available from
 <a href="https://www.maxmind.com">https://www.maxmind.com</a>.
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
@@ -9,11 +10,16 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 type Request struct {
 	Request string
-	Blacklist []string
+	Whitelist []string
 }
 
 type Response struct {
@@ -21,15 +27,51 @@ type Response struct {
 }
 
 func main() {
+	UpdateGeoIP()
 	HandleRequests()
 }
 
-func apiStatic(w http.ResponseWriter, r *http.Request) {
-	reqBody, _ := ioutil.ReadAll(r.Body)
-	json.NewEncoder(w).Encode(reqBody)
-	fmt.Println("Endpoint Hit: homePage")
+//updates the locally stored GeoLite2-Country db
+func UpdateGeoIP() {
+	os.Chdir("./data")
+	cmd := exec.Command("geoipupdate", "1")
+	err := cmd.Run()
+	if err != nil {
+		if fileExists("GeoLite2-Country.mmdb"){
+			log.Println(err.Error())
+			log.Println("Using cached file.")
+		} else {
+			log.Fatal(err.Error())
+		}
+	}
 }
 
+//sets up router and assigns handlers
+func HandleRequests() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/health", healthHandler)
+	router.HandleFunc("/readiness", readinessHandler)
+	router.HandleFunc("/processIpRequest", processIpRequest).Methods("POST")
+
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         ":10000",
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	go func() {
+		log.Println("Starting endpoint")
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	waitForShutdown(srv)
+}
+
+//handler for the ip authorization request
+//request is submitted in json POST body
 func processIpRequest(w http.ResponseWriter, r *http.Request){
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	var request Request
@@ -37,28 +79,14 @@ func processIpRequest(w http.ResponseWriter, r *http.Request){
 	log.Println("Request Recieved for IP: " + request.Request)
 	var country = getIPCountryName(net.ParseIP(request.Request))
 	var response Response
-	response.Authorized = !Contains(request.Blacklist, country)
+	response.Authorized = contains(request.Whitelist, country)
 	json.NewEncoder(w).Encode(response)
 }
 
-func Contains(a []string, x string) bool {
-	for _, n := range a {
-		if x == n {
-			return true
-		}
-	}
-	return false
-}
-
-func HandleRequests() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", apiStatic)
-	router.HandleFunc("/processIpRequest", processIpRequest).Methods("POST")
-	log.Fatal(http.ListenAndServe(":10000", router))
-}
-
+//processes an IP against the geoip2 db
+//returns a string with the plaintext name of the Country the IP is associated with
 func getIPCountryName(ip net.IP) string{
-	db, err := geoip2.Open("data/GeoLite2-Country.mmdb")
+	db, err := geoip2.Open("GeoLite2-Country.mmdb")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -70,4 +98,49 @@ func getIPCountryName(ip net.IP) string{
 	}
 	fmt.Printf("English country name: %v\n", record.Country.Names["en"])
 	return record.Country.Names["en"]
+}
+
+//utility function for finding a string in an array
+func contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+
+//checks for file existence
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+//health handler for Kubernetes
+func healthHandler (w http.ResponseWriter, r *http.Request){
+	w.WriteHeader(http.StatusOK)
+}
+
+//readiness handler for Kubernetes
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func waitForShutdown(srv *http.Server) {
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until we receive our signal.
+	<-interruptChan
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	srv.Shutdown(ctx)
+
+	log.Println("Shutting down")
+	os.Exit(0)
 }
